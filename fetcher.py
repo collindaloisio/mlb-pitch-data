62d3e3b974240fbd96eacd2c751c2e68e89f32d1
Collin D'Aloisio <collindaloisio@gmail.com>
2016-10-14 22:54:38 -0500

    Prompt for date. build url.

--- fetcher.py
+++ fetcher.py
@@ -2,19 +2,52 @@ import urllib
 from ftplib import FTP
 import os.path
 import xml
+import fileinput
+
+
 
 ftpSite = "http://gd2.mlb.com/components/game/mlb/"
 
 ftp = FTP()
 
-print('Enter a Date')
+
+date = raw_input('Enter a date YYYYMMDD: ') #prompt for date
+
+#parse input
+yr= date[0:4]
+mon = date[4:6]
+day = date[6:8]
+
+#build name
+standardName = 'year_'+yr+'/month_'+mon+'/day_'+day
+
+
+
 
 #date = Get some date from the user
 
-fileNames = ftp.nlist('/year_2015/month_03/day_21/')
+#fileNames = ftp.nlist('/year_2015/month_03/day_21/')
+
+#this will be the full Url for some game
+fullUrl = ftpSite+standardName
+
+
 
 
 try:
     urllib.urlretrieve('http://gd2.mlb.com/components/game/mlb/year_2015/month_03/day_21/gid_2015_03_21_kcamlb_chamlb_1/game.xml', 'currentGame.xml')
 except:
     print("Could not Download File")
+
+
+
+try:
+    fileHandle=urllib.urlopen(fullUrl)
+except:
+    print("Could not Download File")
+
+
+print fileHandle.geturl()
+
+
+
