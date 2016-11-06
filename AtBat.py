646d698e04a55e594b172aaaed38df7cab688c6c
Collin D'Aloisio <collindaloisio@gmail.com>
2016-11-06 14:19:36 -0600

    AtBat + Pitch

---
+++ AtBat.py
@@ -0,0 +1,6 @@
+class AtBat :
+	def __init__(self,pitcherId,batterId,outcome,pitches)
+		self.pithcerId = pitcherId
+		self.batterId = batterId
+		self.outcome = outcome
+		self.pitches = pitches
---
+++ Pitch.py
@@ -0,0 +1,7 @@
+class Pitch :
+	def __init__(self,x,y,stats) :
+		self.x = x
+		self.y = y
+		self.stats = stats
+			
+
