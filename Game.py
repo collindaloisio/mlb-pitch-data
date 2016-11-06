e2dd3b4841156d45546d3b294487a984897809e9
Collin D'Aloisio <collindaloisio@gmail.com>
2016-11-06 14:01:16 -0600

    game

--- Game.py
+++ Game.py
@@ -1,6 +1,17 @@
 class Game :
+	def __init__(self,URL,homeTeam,awayTeam,homePitchers,awayPitchers,inningsPlayed) :
+		self.URL = URL
+		self.homeTeam = homeTeam
+		self.awayTeam = awayTeam
+		self.homePitchers = homePitchers
+		self.awayPitchers = awayPitchers
+		self.inningsPlayed = inningsPlayed
+
 
 	class AtBat :
+		import Pitcher
+		import Batter
+		pitcher = Pitcher(self,playerId,)
 		class Pitch :
 			
 
