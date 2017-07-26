d40dd2f3008dc962213b222e4c58b561a3ba9b0f
mathmuser <collindaloisio@gmail.com>
2017-07-25 23:38:50 -0500

    adjusted main for new usage

--- mlblib/main.py
+++ mlblib/main.py
@@ -9,19 +9,25 @@ import scrapeUtils
 
 
 def main():
-    #Below is the usage of the new data batch inserts
+    #Below is the usage of the new single row
     #This requires that you have inning files in your local dir
     #If not, call downloadAllInningFiles with a good YYYYMMDD string first
 
     cluster = Cluster()
     session = cluster.connect('pitch_test')
 
+    #ONLY NEEDS TO HAPPEN ONCE, generates table with allcolumnssss
+    database.generateTableFromDoc(session,settings.docDir+"all_db_columns.txt","pitches_all")
+
     for file in os.listdir(settings.localDir):
         if file.endswith('.xml'):
-            data = scrapeUtils.parsePitchRewrite(settings.localDir + file)
+            data = scrapeUtils.parsePitchRewrite(settings.localDir + file, settings.docDir+"all_db_columns.txt")
             #Batch Insert takes a session so we do not have to open and close cassandra sessions
             #The reason to do multiple batch inserts is so that batches do not get too large
-            database.batchDataInsert(session, data, "pitches")
+            for row in data:
+                database.singleRowInsert(session, row, 'pitches_all')
+
+
 
 
 if __name__ == "__main__":
